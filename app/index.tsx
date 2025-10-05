33c39d4e62822391cb30b7645ff8f7c4c771150f
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-10-05 22:58:33 +0530

    expo linear gradient package and onboarding screen design complete

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,6 @@
+import OnboardingScreen from '@/screens/onboarding';
 import React from 'react';
-import { Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Index() {
-	return (
-		<SafeAreaView>
-			<View className='flex h-full justify-center items-center'>
-				<Text>Index</Text>
-			</View>
-		</SafeAreaView>
-	);
+	return <OnboardingScreen />;
 }
